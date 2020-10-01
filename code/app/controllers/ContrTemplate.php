62bc809a0adb25f24556d7863f95823e0ff7b644
amon.otsuki <Amon@dbsystem-1.us-central1-a.c.csc362-database-287815.internal>
2020-10-01 01:58:16 +0000

    Added MVC templates for each class.

---
+++ code/app/controllers/ContrTemplate.php
@@ -0,0 +1,16 @@
+<?php 
+    include 'models/setModelClassNameHere.php';
+
+    class SomethingsContr {
+        private $trainersModel;
+        public function __construct() {
+            //Make sure you don' put the $ sign in front of the variable name when using $this keyword!
+            //eg:  $this->trainersModel = new TrainersModel();
+        }
+
+        public function someMethod(){
+            return true; //On successful operation, return true.
+        }
+    }
+
+?>
---
+++ code/app/models/ModelTemplate.php
@@ -0,0 +1,24 @@
+<?php
+    include 'models/Database.php';
+    class SomethingsModel extends Database { //Make sure to use plural noun
+        public function someMethod(){
+            $sql = //SQL for prepared statement;
+            $stmt;
+            if (!$stmt = $this->connect()->prepare($sql)){
+                echo "Prepare statement failed<br>";
+            }
+            if (!$stmt->bind_param("s", $name)){
+                echo "Parameter binding failed<br>";
+            }
+            if (!$stmt->execute()){
+                echo "Query execution failed<br>";
+            }
+            $result = $stmt->get_result(); //Get mysqli_result object.
+
+            //We should probably always return $result(mysqli_result object) so that we can make all 
+            // views following that coding rule.
+            // Doc for mysqli_result object: https://www.php.net/manual/en/class.mysqli-result.php
+            return $result;
+        }
+    }
+?>
